730421bb49a144c1ca7047dc2dbd95d8a269054c
Joao Duarte <joao.duarte@maxmilhas.com.br>
2022-10-21 17:53:43 -0300

    build: add dependencies

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+    roots: ['<rootDir>/src'],
+    clearMocks: true,
+    collectCoverage: false,
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+    transform: {
+        '.+\\.ts$': 'ts-jest'
+    },
+};
