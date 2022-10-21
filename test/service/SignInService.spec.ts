950e5ea58ae314bcf24c337b9fdfc0426581aec9
Joao Duarte <joao.duarte@maxmilhas.com.br>
2022-10-21 18:37:00 -0300

    refactor: create sut for SignInService

--- test/service/SignInService.spec.ts
+++ test/service/SignInService.spec.ts
@@ -1,17 +1,26 @@
 import { SignInService } from '../../src/service/SignInService';
 import { Login } from '../../src/service/Login';
+import { SignInServiceInterface } from '../../src/service/SignInServiceInterface';
+import { SignInRepository } from '../../src/repository/SignInRepository';
+
 describe('SignInService', () => {
+    let sut: SignInServiceInterface;
+    let signInRepository: SignInRepository;
+
+    beforeEach(() => {
+        signInRepository = {
+            find: (login: Login) => Promise.resolve({ user: 'any_user', password: 'any_password' })
+        }
+        sut = new SignInService(signInRepository);
+    })
+
     it('Should return false when user is invalid', async () => {
         const login = {
             user: 'any_user',
             password: 'any_password'
         }
-        const signInRepository = {
-            find: (login: Login) => Promise.resolve({ user: 'any_user', password: 'any_password' })
-        }
-        const service = new SignInService(signInRepository);
 
-        const result = await service.execute(login);
+        const result = await sut.execute(login);
 
         expect(result).toBeFalsy();
     });
